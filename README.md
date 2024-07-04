7093bf37f3e1f5574104297c613820a1a527c2de
Nguyen Minh Duc <nminhducit@gmail.com>
2024-07-04 10:50:16 +0700

    Update MD file and Batch file

---
+++ README.md
@@ -0,0 +1,49 @@
+# CleanMaster by NMINHDUCIT
+
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Features](#features)
+3. [System Requirements](#system-requirements)
+4. [Installation](#installation)
+5. [Usage](#usage)
+6. [Contributing](#contributing)
+7. [License](#license)
+
+## Introduction
+**CleanMaster by NMINHDUCIT** is a batch script utility designed to help you clean up unnecessary files from your Windows PC, freeing up disk space and improving system performance. 
+
+This tool offers options to delete internet cookies, temporary internet files, perform disk cleanup, and defragment your hard drives.
+
+## Features
+- **Delete Internet Cookies**: Removes cookies stored by your web browsers.
+- **Delete Temporary Internet Files**: Cleans up temporary files used by browsers and other applications.
+- **Disk Cleanup**: Deletes temporary and unnecessary files from various system directories.
+- **Disk Defragment**: Defragments all hard drives to improve file access speed.
+
+## System Requirements
+- Windows 7, 8, 10, or 11
+- Administrative privileges to run the script
+- Internet Explorer or Edge (for deleting cookies and temporary internet files)
+
+## Installation
+1. Download the `main.bat` file from the [src](#).
+2. Place the `main.bat` file in a convenient location on your PC.
+
+## Usage
+1. Right-click on the `main.bat` file and select **Run as administrator**.
+2. You will see a menu with options:
+    - `[1] Delete Internet Cookies`
+    - `[2] Delete Temporary Internet Files`
+    - `[3] Disk Cleanup`
+    - `[4] Disk Defragment`
+    - `[5] Exit`
+3. Enter the number corresponding to the task you want to perform and press **Enter**.
+
+### Screenshots
+![screenshot](https://path_to_screenshot_image)
+
+## Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on [GitHub](https://github.com/NMINHDUCIT/CleanMaster).
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/main.bat
@@ -0,0 +1,122 @@
+@echo off
+title PC Cleanup Utility by NMINHDUCIT
+
+:menu
+cls
+echo --------------------------------------------------------------------------------
+echo PC Cleanup Utility
+echo --------------------------------------------------------------------------------
+echo.
+echo Select a tool
+echo =============
+echo.
+echo [1] Delete Internet Cookies
+echo [2] Delete Temporary Internet Files
+echo [3] Disk Cleanup
+echo [4] Disk Defragment
+echo [5] Exit
+echo.
+set /p op=Run: 
+if %op%==1 goto delete_cookies
+if %op%==2 goto delete_temp_files
+if %op%==3 goto disk_cleanup
+if %op%==4 goto disk_defrag
+if %op%==5 goto exit
+goto error
+
+:delete_cookies
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Internet Cookies
+echo --------------------------------------------------------------------------------
+echo.
+echo Deleting Cookies...
+ping localhost -n 3 >nul
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Internet Cookies
+echo --------------------------------------------------------------------------------
+echo.
+echo Cookies deleted.
+echo.
+echo Press any key to return to the menu. . .
+pause >nul
+goto menu
+
+:delete_temp_files
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Temporary Internet Files
+echo --------------------------------------------------------------------------------
+echo.
+echo Deleting Temporary Internet Files...
+ping localhost -n 3 >nul
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+cls
+echo --------------------------------------------------------------------------------
+echo Delete Temporary Internet Files
+echo --------------------------------------------------------------------------------
+echo.
+echo Temporary Internet Files deleted.
+echo.
+echo Press any key to return to the menu. . .
+pause >nul
+goto menu
+
+:disk_cleanup
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Cleanup
+echo --------------------------------------------------------------------------------
+echo.
+echo Running Disk Cleanup...
+ping localhost -n 3 >nul
+if exist "C:\WINDOWS\temp" del /f /q "C:\WINDOWS\temp\*.*" 2>nul
+if exist "C:\WINDOWS\tmp" del /f /q "C:\WINDOWS\tmp\*.*" 2>nul
+if exist "C:\tmp" del /f /q "C:\tmp\*.*" 2>nul
+if exist "C:\temp" del /f /q "C:\temp\*.*" 2>nul
+if exist "%temp%" del /f /q "%temp%\*.*" 2>nul
+if exist "%tmp%" del /f /q "%tmp%\*.*" 2>nul
+for /D %%G in ("%temp%\*") do rd /s /q "%%G" 2>nul
+for /D %%G in ("%tmp%\*") do rd /s /q "%%G" 2>nul
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Cleanup
+echo --------------------------------------------------------------------------------
+echo.
+echo Disk Cleanup successful!
+echo.
+pause
+goto menu
+
+:disk_defrag
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Defragment
+echo --------------------------------------------------------------------------------
+echo.
+echo Defragmenting hard disks...
+ping localhost -n 3 >nul
+defrag -c -v
+cls
+echo --------------------------------------------------------------------------------
+echo Disk Defragment
+echo --------------------------------------------------------------------------------
+echo.
+echo Disk Defrag successful!
+echo.
+pause
+goto menu
+
+:error
+cls
+echo Command not recognized.
+ping localhost -n 4 >nul
+goto menu
+
+:exit
+cls
+echo Thanks for using PC Cleanup Utility by NMINHDUCIT Github
+ping localhost -n 3 >nul
+exit
